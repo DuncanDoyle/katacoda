bc3a81055016b47354c82858bf0b02750271b900
Duncan Doyle <Duncan.Doyle@gmail.com>
2017-06-26 10:18:28 +0200

    More documentation.

--- brms-loan-application-on-openshift/creating-a-rules-project/env-init.sh
+++ brms-loan-application-on-openshift/creating-a-rules-project/env-init.sh
@@ -1,4 +1,4 @@
-#ssh root@host01 "echo 'Starting JBoss BRMS docker image.'"
-#echo "This can take a couple of minutes while the image is being fetched from Docker Hub."
-#docker run -d -p 8080:8080 --name=jboss-brms-6.4 -e "JAVA_OPTS=-Xms768m -Xmx768m -XX:MetaspaceSize=96M -XX:MaxMetaspaceSize=256m -Djava.net.preferIPv4Stack=true -Djboss.modules.system.pkgs=$JBOSS_MODULES_SYSTEM_PKGS -Djava.awt.headless=true" duncandoyle/jboss-brms:6.4
-#echo "JBoss BRMS started."
+echo "We will not be using the terminal in this scenario."
+echo "Atm, Katacoda does not provide the resources required to run JBoss BRMS Demo Central."
+
+
